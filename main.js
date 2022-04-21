b2f5dd7eb9f042437e83d911867c6089919b0367
OlivierFch <foucher.olivier@dbmail.com>
2022-04-21 21:01:49 +0200

    Seventh commit
    Valeur section :
    Utilisation de owlCarousel
    
    Changer les images

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 50,
+    autoplay:true,
+    autoplayTimeout:10000,
+    nav: true,
+    responsive: {
+        600: {
+            items: 1
+        }
+    }
+})
